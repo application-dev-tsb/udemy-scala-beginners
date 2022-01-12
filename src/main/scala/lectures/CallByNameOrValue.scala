6dae9c71d7c00fda37dd5da87476870e5f58b227
Lyndon Bibera <lyndon.bibera@headhuntr.io>
2022-01-12 14:06:25 +0800

    Basics: call by value (eager) VS. call by name (lazy)

---
+++ src/main/scala/lectures/CallByNameOrValue.scala
@@ -0,0 +1,19 @@
+package lectures
+
+import scala.util.Random
+
+object CallByNameOrValue extends App {
+
+  def callByValue(myValue: Int) = {
+    println(s"Call By Value: $myValue")
+    println(s"Call By Value: $myValue")
+  }
+
+  def callByName(supplier: => Int) = {
+    println(s"Call By Name: $supplier")
+    println(s"Call By Name: $supplier")
+  }
+
+  callByValue(Random.nextInt())
+  callByName(Random.nextInt())
+}
