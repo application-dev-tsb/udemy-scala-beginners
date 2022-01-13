73b3758285f628c2541db9dabf50e38e526d0b58
Lyndon Bibera <lyndon.bibera@headhuntr.io>
2022-01-13 18:18:37 +0800

    OOP: inheritance with values

--- src/main/scala/lectures/oop/Inheritance.scala
+++ src/main/scala/lectures/oop/Inheritance.scala
@@ -11,4 +11,23 @@ object Inheritance extends App {
 
   val cat = new Cat
   cat.eat
+
+  sealed abstract class Robot(val name: String, val powerLevel: Int) {
+    def attack(): Unit
+
+    final def robotTalk() = println(s"$name: Talk Robotiiiikkk")
+
+    override def toString: String = s"Robot($name, $powerLevel)"
+  }
+
+  class NinjaBot(name: String, powerLevel: Int) extends Robot(name, powerLevel) {
+    override def attack(): Unit = println("Attack! Ninja Style")
+  }
+
+  val ninjaBot = new NinjaBot("Ninja 1", 9)
+
+  //FINAL = disallow override/extend
+  //SEALED = same with FINAL but allows when defined in the same file
+
+  println(ninjaBot)
 }
