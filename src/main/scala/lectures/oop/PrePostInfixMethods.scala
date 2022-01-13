a929e5539781e21de32fb5605c99091f5be04f0b
Lyndon Bibera <lyndon.bibera@headhuntr.io>
2022-01-13 10:45:42 +0800

    OOP: Syntactic sugar for methods

---
+++ src/main/scala/lectures/oop/PrePostInfixMethods.scala
@@ -0,0 +1,41 @@
+package lectures.oop
+
+import scala.language.postfixOps //required for postfix ops
+
+object PrePostInfixMethods extends App {
+
+
+  class Person(val name: String, val favoriteLanguage: String) {
+
+    /**
+     * unary operators: - ~ ! +
+     */
+    def unary_! = s"$favoriteLanguage is so Awesome"
+
+    def likesTheSameLanguageAs(otherPerson: Person): Boolean = favoriteLanguage == otherPerson.favoriteLanguage
+
+    /**
+     * scala is a bit liberal with method names
+     */
+    def +(otherPerson: Person): Person = new Person(s"${this.name}, ${otherPerson.name}", this.favoriteLanguage)
+
+    def isModern: Boolean = favoriteLanguage == "Scala"
+  }
+
+  val james = new Person("James Gosling", "Java")
+  val steve = new Person("Steve Jobs", "ObjectiveC")
+
+  println(!steve)
+
+  /**
+   * we can skip the "." and use a more expressive syntax
+   * this only works when theres just one parameter
+   */
+  println(james likesTheSameLanguageAs steve)
+  println(james + steve)
+
+  /**
+   * not that cool but still possible: postfix
+   */
+  println(steve isModern)
+}
