704cb00bcf5a59ed3462be661c30d632de7e6d98
Lyndon Bibera <lyndon.bibera@headhuntr.io>
2022-01-17 17:38:33 +0800

    Functional: Tuple

--- src/main/scala/lectures/functional/TuplesAndMaps.scala
+++ src/main/scala/lectures/functional/TuplesAndMaps.scala
@@ -4,4 +4,11 @@ object TuplesAndMaps extends App {
 
   val myTuple = Tuple2(2, "Hello World") // (Int, String)
   val myBetterTuple =  (2, "Hello Better World")
+
+  //things you can do on tuples
+  println(myBetterTuple._1) //2
+  println(myBetterTuple.copy(_2 = "By Java!"))
+  println(myBetterTuple.swap)
+
+
 }
