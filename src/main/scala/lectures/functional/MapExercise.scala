08475d5bedac7a3d25f682a9a44e0c9fcfade474
Lyndon Bibera <lyndon.bibera@headhuntr.io>
2022-01-18 07:39:46 +0800

    Functional: exercise for map - setup

---
+++ src/main/scala/lectures/functional/MapExercise.scala
@@ -0,0 +1,20 @@
+package lectures.functional
+
+object MapExercise extends App {
+
+  /**
+   * overly simplified social network:
+   * Person = String
+   * - add a person to the network
+   * - remove
+   * - friend (mutual)
+   * - unfriend
+   *
+   * - number of friends
+   * - person with most friends
+   * - how many people have no friends
+   * - if there is a social connection between two people
+   */
+
+
+}
