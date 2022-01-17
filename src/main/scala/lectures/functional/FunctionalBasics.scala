48d415a24bae7ce7750d2080220418a93fc1d2ac
Lyndon Bibera <lyndon.bibera@headhuntr.io>
2022-01-17 10:18:15 +0800

    Functional: Higher order functions

--- src/main/scala/lectures/functional/FunctionalBasics.scala
+++ src/main/scala/lectures/functional/FunctionalBasics.scala
@@ -1,5 +1,16 @@
 package lectures.functional
 
-object FunctionalBasics {
+object FunctionalBasics extends App {
+
+  //higher-order functions
+  def nTimes[A](f: (A) => A, n: Int, value: A): A = {
+    if (n <= 0) value
+    else nTimes(f, n-1, f(value))
+  }
+
+  val add3 = (x: Int) => x + 3
+
+  println(nTimes(add3, 1, 3))
+  println(nTimes(add3, 10, 3))
 
 }
