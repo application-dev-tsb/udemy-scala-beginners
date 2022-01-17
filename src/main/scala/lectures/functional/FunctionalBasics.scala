787b50ba99ad238c2220c9f5ed13c5e529174899
Lyndon Bibera <lyndon.bibera@headhuntr.io>
2022-01-17 10:28:53 +0800

    Functional: Higher order functions

--- src/main/scala/lectures/functional/FunctionalBasics.scala
+++ src/main/scala/lectures/functional/FunctionalBasics.scala
@@ -13,4 +13,14 @@ object FunctionalBasics extends App {
   println(nTimes(add3, 1, 3))
   println(nTimes(add3, 10, 3))
 
+
+  def nTimesHOF[A](f: (A) => A, n: Int): (A) => A = {
+    if (n <= 0) (x: A) => x
+    else (x: A) => nTimesHOF(f, n-1)(f(x))
+  }
+
+  val add3HOF = nTimesHOF(add3, 10)
+
+  println(add3HOF(3))
+
 }
