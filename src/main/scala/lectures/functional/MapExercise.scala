ad3892b77927e7de69db5980224cfd6c9da0b06c
Lyndon Bibera <lyndon.bibera@headhuntr.io>
2022-01-18 08:24:35 +0800

    Functional: exercise for map - basic add and friend methods

--- src/main/scala/lectures/functional/MapExercise.scala
+++ src/main/scala/lectures/functional/MapExercise.scala
@@ -18,8 +18,26 @@ object MapExercise extends App {
 
   //our definition
   type Person = String
-  type SocialNetwork = Map[Person, List[Person]]
+  type SocialNetwork = Map[Person, Set[Person]]
   var socialNetwork: SocialNetwork = Map()
 
-  def addPerson(network: SocialNetwork, person: Person): SocialNetwork = ???
+  def addPerson(network: SocialNetwork, person: Person): SocialNetwork = network + (person -> Set())
+
+  def friend(network: SocialNetwork, a: Person, b: Person): SocialNetwork = {
+    val friendsOfA = network(a)
+    val friendsOfB = network(b)
+
+    network + (a -> (friendsOfA + b)) + (b -> (friendsOfB + a))
+  }
+
+  socialNetwork = addPerson(socialNetwork, "Dave")
+  socialNetwork = addPerson(socialNetwork, "Kevin")
+  socialNetwork = addPerson(socialNetwork, "Levi")
+
+  println(socialNetwork)
+
+  socialNetwork = friend(socialNetwork, "Levi", "Kevin")
+
+  println(socialNetwork)
+
 }
