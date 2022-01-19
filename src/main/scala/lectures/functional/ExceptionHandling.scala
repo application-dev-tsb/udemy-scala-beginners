21d11d5967d31c7b4dd3c506e07c906508445f6c
Lyndon Bibera <lyndon.bibera@headhuntr.io>
2022-01-19 09:40:14 +0800

    Functional: sample try

--- src/main/scala/lectures/functional/ExceptionHandling.scala
+++ src/main/scala/lectures/functional/ExceptionHandling.scala
@@ -1,5 +1,19 @@
 package lectures.functional
 
+import scala.util.{Failure, Success, Try}
+
 object ExceptionHandling extends App {
+
+  def unsafeMethod(throwError: Boolean): String = if (throwError) throw new RuntimeException("Throw an error") else "OK"
+
+  def safeMethod(throwError: Boolean): Try[String] = Try(unsafeMethod(throwError))
+
+  def backupMethod(): Try[String] = Success("OK Backup")
+
+  val fallback: Try[String] = Try(unsafeMethod(true)) orElse backupMethod()
+  val betterFallback: Try[String] = safeMethod(true) orElse backupMethod()
+
+
+
 
 }
