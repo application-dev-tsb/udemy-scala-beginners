38f69adecd8e6a48a7cce211fdd9226a03708e87
Lyndon Bibera <lyndon.bibera@headhuntr.io>
2022-01-19 10:11:55 +0800

    Functional: simple pattern matching

--- src/main/scala/lectures/functional/PatternMatching.scala
+++ src/main/scala/lectures/functional/PatternMatching.scala
@@ -1,5 +1,17 @@
 package lectures.functional
 
+import scala.util.Random
+
 object PatternMatching extends App {
 
+  val rand = new Random
+  val x = rand.nextInt(10)
+
+  val description = x match {
+    case 1 => "The One"
+    case 2 => "double whammy!"
+    case _ => "Default thing"
+  }
+
+  println(s"$x : $description")
 }
