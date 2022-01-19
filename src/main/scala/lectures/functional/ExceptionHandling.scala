53d0cd06cdd7d10a0fb1d49b6be9b2b2aa96b258
Lyndon Bibera <lyndon.bibera@headhuntr.io>
2022-01-19 09:49:13 +0800

    Functional: using for comprehensions

--- src/main/scala/lectures/functional/ExceptionHandling.scala
+++ src/main/scala/lectures/functional/ExceptionHandling.scala
@@ -14,6 +14,25 @@ object ExceptionHandling extends App {
   val betterFallback: Try[String] = safeMethod(true) orElse backupMethod()
 
 
+  val configOptions: Map[String, String] = Map(
+    "url" -> "https://localhost",
+    "port" -> "8080"
+  )
 
+  //the hard way
+  val maybeUrl = Try(configOptions("url"))
+  val maybePort = Try(configOptions("port"))
+  val maybeConnection: Try[String] = maybeUrl.flatMap(url => maybePort.flatMap(port => Success(s"$url:$port")))
+  maybeConnection.foreach(println)
+
+  //using for comprehension
+  val maybeConnectionBetter = for {
+    url <- Try(configOptions("url"))
+    port <- Try(configOptions("port"))
+  } yield {
+    s"$url:$port"
+  }
+
+  maybeConnectionBetter.foreach(println)
 
 }
