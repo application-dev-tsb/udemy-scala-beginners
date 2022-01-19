6c96354e5f0ab02fd5c50108df6e68140371fd28
Lyndon Bibera <lyndon.bibera@headhuntr.io>
2022-01-19 10:54:28 +0800

    Functional: pattern matching a little more advanced

--- src/main/scala/lectures/functional/PatternMatching.scala
+++ src/main/scala/lectures/functional/PatternMatching.scala
@@ -28,4 +28,44 @@ object PatternMatching extends App {
   }
 
   println(greeting)
+
+  //advance: pattern matching on sealed hierarchies
+  sealed class Animal
+  case class Dog(bark: String) extends Animal
+  case class Parrot(greeting: String) extends Animal
+
+  val animal: Animal =
+    if (new Random().nextBoolean()) Dog("woof woof")
+    else Parrot("Hello Human!")
+
+  val animalGreeting: String = animal match {
+    case Dog(b) => s"DOG: $b"
+    case Parrot(words) => s"HUMAN VOICE: $words"
+    case _ => "Scary unknown animal"
+  }
+
+  println(animalGreeting)
+
+  trait Expr
+  case class Number(n: Int) extends Expr
+  case class Sum(e1: Expr, e2: Expr) extends Expr
+  case class Prod(e1: Expr, e2: Expr) extends Expr
+
+  def show(expr: Expr): String = expr match {
+    case Number(n) => s"$n"
+    case Sum(e1, e2) => show(e1) + " + " + show(e2)
+    case Prod(e1, e2) => {
+      def maybeShowParenthesis(expr: Expr) = expr match {
+        case Prod(_, _) => show(expr)
+        case Number(_) => show(expr)
+        case _ => s"( ${show(expr)} )"
+      }
+
+      maybeShowParenthesis(e1) + " x " + maybeShowParenthesis(e2)
+    }
+    case _ => "?"
+  }
+
+  println(show(Sum(Number(1), Number(2))))
+  println(show(Sum(Number(1), Prod(Number(1), Sum(Number(4), Number(5))))))
 }
