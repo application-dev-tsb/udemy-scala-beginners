cd3f397d1ecb6f939c63d32f40223a8cbf2db75a
Lyndon Bibera <lyndon.bibera@headhuntr.io>
2022-01-19 10:19:03 +0800

    Functional: pattern matching and value extraction

--- src/main/scala/lectures/functional/PatternMatching.scala
+++ src/main/scala/lectures/functional/PatternMatching.scala
@@ -7,6 +7,7 @@ object PatternMatching extends App {
   val rand = new Random
   val x = rand.nextInt(10)
 
+  //simple example:
   val description = x match {
     case 1 => "The One"
     case 2 => "double whammy!"
@@ -14,4 +15,17 @@ object PatternMatching extends App {
   }
 
   println(s"$x : $description")
+
+  //advanced: pattern matching can extract values
+  case class Person(name: String, age: Int)
+  val bob = Person("Bob", 25)
+  val mary = Person("Mary", 18)
+
+  val greeting: String = bob match {
+    case Person(n, a) if a < 21 => s"$n is too young"
+    case Person(n, a) => s"$n is $a years old"
+    case _ => "No Match!" //exception if a case is not matched
+  }
+
+  println(greeting)
 }
